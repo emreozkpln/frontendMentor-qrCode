c402d2528ea7539a62ac019aeda55a98d9d82d90
emreozkpln <emre.ozkaplan@hotmail.com>
2023-03-26 02:40:08 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import img from "./img/image-qr-code.png"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid place-content-center">
+      <div className="bg-white flex flex-col justify-center text-center w-[300px] h-[490px] rounded-lg shadow-md shadow-gray-400 p-3 m-24 gap-5 ">
+        <img className="rounded-lg h-[275px] object-cover" src={img} />
+        <div className="gap-4 grid">
+          <div className="font-bold text-xl">Improve your front-end<br /> skills by building projects</div>
+          <div className=" text-gray-500 mb-5">Scan the QR code to visit Frontend <br /> Mentor and take your coding skills to the next level</div>
+        </div>
+      </div>
     </div>
   );
 }
